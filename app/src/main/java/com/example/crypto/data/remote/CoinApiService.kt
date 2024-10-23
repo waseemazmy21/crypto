cb151118639582fb67c3b79f5a5c82e2d780c467
Waseem Azmy <waseemazmy21@gmail.com>
2024-10-23 11:45:26 +0300

    Add coin api service interface

--- app/src/main/java/com/example/crypto/data/remote/CoinApiService.kt
+++ app/src/main/java/com/example/crypto/data/remote/CoinApiService.kt
@@ -1,8 +1,14 @@
 package com.example.crypto.data.remote
 
+import com.example.crypto.data.remote.dto.CoinDetailsDto
+import com.example.crypto.data.remote.dto.CoinDto
 import retrofit2.http.GET
+import retrofit2.http.Path
 
 interface CoinApiService {
     @GET("/v1/coins")
-    suspend fun getCoins()
+    suspend fun getCoins(): List<CoinDto>
+
+    @GET("/v1/coins/id")
+    suspend fun getCoin(@Path("id") id:String): CoinDetailsDto
 }

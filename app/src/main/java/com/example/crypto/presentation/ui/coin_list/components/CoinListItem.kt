6f2445613c4c3bf47795a304006c4f267aea4773
Waseem Azmy <waseemazmy21@gmail.com>
2024-10-28 16:36:26 +0300

    Add CoinListItem and CoinLIstScreen

---
+++ app/src/main/java/com/example/crypto/presentation/ui/coin_list/components/CoinListItem.kt
@@ -0,0 +1,41 @@
+package com.example.crypto.presentation.ui.coin_list.components
+
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.unit.dp
+import com.example.crypto.domain.model.Coin
+
+@Composable
+fun CoinListItem(
+    coin: Coin,
+    onClick: (coin: Coin) -> Unit,
+) {
+    Row (
+        modifier = Modifier.fillMaxWidth()
+            .padding(20.dp)
+            .clickable { onClick(coin) },
+        horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "${coin.rank}. ${coin.name} (${coin.symbol})",
+            style = MaterialTheme.typography.bodyLarge
+        )
+        Text(
+            text = if (coin.isActive) "active" else "inactive",
+            color = if(coin.isActive) Color.Green else Color.Red,
+            fontStyle = FontStyle.Italic,
+            style = MaterialTheme.typography.bodyMedium,
+        )
+    }
+}

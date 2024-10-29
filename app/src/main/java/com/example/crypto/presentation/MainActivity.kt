cc56a6d186ad8fc1df1d155ef8da276f2bc8badb
Waseem Azmy <waseemazmy21@gmail.com>
2024-10-29 16:02:48 +0300

    Apply content padding

--- app/src/main/java/com/example/crypto/presentation/MainActivity.kt
+++ app/src/main/java/com/example/crypto/presentation/MainActivity.kt
@@ -1,47 +1,52 @@
-package com.example.crypto
+package com.example.crypto.presentation
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.crypto.presentation.ui.coin_details.CoinDetailsScreen
+import com.example.crypto.presentation.ui.coin_list.CoinListScreen
 import com.example.crypto.ui.theme.CryptoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @OptIn(ExperimentalLayoutApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             CryptoTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                val navController = rememberNavController()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    content = { contentPadding ->
+                        NavHost(
+                            navController = navController,
+                            startDestination = "coinsList",
+                            modifier = Modifier.padding(contentPadding)
+                        ) {
+                            composable(route = "coinsList") {
+                                CoinListScreen(navController = navController)
+                            }
+
+                            composable(route = "coinDetails/{id}") {
+                                    CoinDetailsScreen()
+                            }
+                        }
+                    }
+                )
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoTheme {
-        Greeting("Android")
     }
 }
